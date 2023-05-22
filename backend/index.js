b76ddea2ca41c80162376a3b1d59b00a45d1c401
Akanksha987 <agrawalakanksha23123@gmail.com>
2023-05-22 17:05:20 +0530

    Updated backendand removed some files

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-// const http = require("http");
 const BookRoute = require("./routes/booksRoute");
 const CartRoute = require("./routes/cartRoute");
-const ChatRoute = require("./routes/chatRoute");
-const MessageRoute = require("./routes/messageRoute");
 const app = express();
 const cors = require("cors");
 const DB = process.env.DB;
@@ -15,9 +12,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 mongoose
   .connect(DB, { family: 4 })
-  .then(() => app.listen(3009, () => console.log(`Listening at Port 3009`)))
+  .then(() => app.listen(3004, () => console.log(`Listening at Port 3004`)))
   .catch((error) => console.log(`${error} did not connect`));
 app.use("/api", BookRoute);
 app.use("/api", CartRoute);
-app.use("/chat", ChatRoute);
-app.use("/message", MessageRoute);
